7cfd5f371d3111ae332e3166df4e928f994282fa
gowthamarya <gowtham.arya999@gmail.com>
2025-09-28 14:22:27 +0530

    fixed build errors in production...

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js';
 
 const supabase = createClient(
   process.env.APPLE_DB_SUPABASE_URL!,
-  process.env.APPLE_DB_APPLE_SUPABASESUPABASE_ANON_KEY!
+  process.env.APPLE_DB_APPLE_SUPABASE_SUPABASE_ANON_KEY!
 );
 
 function createIncludesQuery(includeRelations: string[]): string {
