d55811f8a037f5a870d17142a8566230e51a5cf9
gowthamarya <gowtham.arya999@gmail.com>
2025-09-28 14:43:02 +0530

    fixed buld errors..

--- lib/supabaseClient.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabase = createClient(
-  process.env.APPLE_DB_SUPABASE_URL!,
-  process.env.APPLE_DB_APPLE_SUPABASE_SUPABASE_ANON_KEY!
-);
-
-export default supabase;
