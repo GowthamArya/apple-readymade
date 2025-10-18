005a1cbfae0450539e0d7927446b094d4039d9fb
gowthamarya <gowtham.arya999@gmail.com>
2025-10-18 23:18:29 +0530

    fixed build errors

--- app/api/generic/[entityname]/route.ts
+++ app/api/generic/[entityname]/route.ts
@@ -2,51 +2,51 @@ import { EntityMapping } from "@/utils/supabase/entitymapping";
 import GenericRepo from "@/utils/supabase/genericRepo";
 import { NextResponse } from "next/server";
 
-interface Params {
-  params: { entityname: string; id?: string };
-}
-
-export async function POST(req: Request, { params }: Params) {
+export async function POST(
+  req: Request,
+  { params }: { params: Promise<{ entityname: string; id?: string }> }
+) {
   try {
     const { entityname } = await params;
 
     if (!(entityname in EntityMapping)) {
-      throw new Error("Invalid entity name");
+      throw new Error('Invalid entity name');
     }
 
     const requestData = await req.json();
-
     const genericRepo = new GenericRepo<typeof requestData>(entityname);
-
     const createdEntity = await genericRepo.create(requestData);
 
     return NextResponse.json({ createdEntity });
   } catch (err: any) {
-    return NextResponse.json(
-      { error: err.message || "Failed to process request" },
-      { status: 500 }
-    );
+    return NextResponse.json({ error: err.message || 'Failed to process request' }, { status: 500 });
   }
 }
 
-export async function GET(req: Request, { params }: {params: { entityname: string }} ) {
+
+export async function GET(
+  req: Request,
+  { params }: { params: Promise<{ entityname: string }> }
+) {
   try {
     const { entityname } = await params;
+
     if (!(entityname in EntityMapping)) {
-      throw new Error("Invalid entity name");
+      throw new Error('Invalid entity name');
     }
 
     const allEntities = await GenericRepo.fetchAll(entityname);
     return NextResponse.json({ data: allEntities });
   } catch (err: any) {
-    return NextResponse.json(
-      { error: err.message || "Failed to fetch data" },
-      { status: 500 }
-    );
+    return NextResponse.json({ error: err.message || 'Failed to fetch data' }, { status: 500 });
   }
 }
 
-export async function PUT(req: Request, { params }: Params) {
+
+export async function PUT(
+  req: Request,
+  { params }: { params: Promise<{ entityname: string; id?: string }> }
+) {
   try {
     const { entityname, id } = await params;
 
@@ -56,21 +56,19 @@ export async function PUT(req: Request, { params }: Params) {
     if (!id) throw new Error("ID parameter required for update");
 
     const partialData = await req.json();
-
     const genericRepo = new GenericRepo<typeof partialData>(entityname, id);
-
     await genericRepo.update(partialData);
 
     return NextResponse.json({ message: "Update successful" });
   } catch (err: any) {
-    return NextResponse.json(
-      { error: err.message || "Failed to update data" },
-      { status: 500 }
-    );
+    return NextResponse.json({ error: err.message || "Failed to update data" }, { status: 500 });
   }
 }
 
-export async function DELETE(req: Request, { params }: Params) {
+export async function DELETE(
+  req: Request,
+  { params }: { params: Promise<{ entityname: string; id?: string }> }
+) {
   try {
     const { entityname, id } = await params;
 
@@ -80,14 +78,10 @@ export async function DELETE(req: Request, { params }: Params) {
     if (!id) throw new Error("ID parameter required for delete");
 
     const genericRepo = new GenericRepo(entityname, id);
-
     await genericRepo.delete();
 
     return NextResponse.json({ message: "Deletion successful" });
   } catch (err: any) {
-    return NextResponse.json(
-      { error: err.message || "Failed to delete data" },
-      { status: 500 }
-    );
+    return NextResponse.json({ error: err.message || "Failed to delete data" }, { status: 500 });
   }
 }
