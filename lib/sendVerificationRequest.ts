b2b4845f98ffc0f59a896efaa02b3f1d1fb951b3
Gowtham Arya <gowtham.arya999@gmail.com>
2025-11-24 12:08:13 +0530

    fixed production bug...

--- lib/sendVerificationRequest.ts
+++ lib/sendVerificationRequest.ts
@@ -1,9 +1,9 @@
-import SibApiV3Sdk from "@sendinblue/client";
 import { render } from "@react-email/render";
 import VerificationEmail from "@/emails/VerificationEmail";
 
 export async function sendVerificationRequest({ identifier: email, url } : any) {
   try {
+    const SibApiV3Sdk = require("@sendinblue/client");
     const client = new SibApiV3Sdk.TransactionalEmailsApi();
     client.setApiKey(SibApiV3Sdk.TransactionalEmailsApiApiKeys.apiKey, process.env.BREVO_API_KEY!);
 
