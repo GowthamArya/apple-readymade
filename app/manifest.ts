e33db5ab85ae09971a414a8ee685e65cd30a0178
Gowtham Arya <gowtham.arya999@gmail.com>
2025-11-29 14:31:16 +0530

    feat: Add web app manifest file for PWA configuration.

--- app/manifest.ts
+++ app/manifest.ts
@@ -1,25 +1,25 @@
 import type { MetadataRoute } from 'next'
- 
+
 export default function manifest(): MetadataRoute.Manifest {
   return {
     name: "Apple Readymade",
-    short_name: 'AppleReadymade',
+    short_name: 'Apple Readymade',
     description: '"Apple Readymade" - Your go-to app for buying and selling Apple products with ease and confidence.',
     start_url: '/',
     display: 'standalone',
     background_color: '#000000',
     theme_color: "#3A6F43",
     icons: [
-        {
-            "src": "/Icons/logo-192x192.png",
-            "sizes": "192x192",
-            "type": "image/png"
-        },
-        {
-            "src": "/Icons/logo-512x512.png",
-            "sizes": "512x512",
-            "type": "image/png"
-        }
+      {
+        "src": "/Icons/logo-192x192.png",
+        "sizes": "192x192",
+        "type": "image/png"
+      },
+      {
+        "src": "/Icons/logo-512x512.png",
+        "sizes": "512x512",
+        "type": "image/png"
+      }
     ],
   }
 }
